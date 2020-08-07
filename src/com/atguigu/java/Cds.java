1fd2f805b7d7818226260a87895293c9d0242d22
wu-jb <879012831@qq.com>
2020-08-07 22:16:19 +0800

    Initial commit

---
+++ src/com/atguigu/java/Cds.java
@@ -0,0 +1,9 @@
+package com.atguigu.java;
+
+/*
+ *@author wu_jb
+ *@date 2020/8/7
+ */
+public class Cds {
+
+}
---
+++ src/com/atguigu/java/One.java
@@ -0,0 +1,12 @@
+package com.atguigu.java;
+
+public class One {
+    public static void main(String[] args){
+        System.out.println("hello world!!!!");
+    }
+    public void say(){
+
+    }
+
+
+}
